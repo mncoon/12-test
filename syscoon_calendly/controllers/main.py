5a3302fcfccfdb0920f612eb80da82a101dc9406
Mathias Neef <mathias.neef@syscoon.com>
2020-09-07 20:31:00 +0200

    [UPD] add new module

--- syscoon_calendly/controllers/main.py
+++ syscoon_calendly/controllers/main.py
@@ -1,6 +1,8 @@
 #See LICENSE file for full copyright and licensing details.
 
 import logging
+import requests
+import json
 
 from odoo import http
 
@@ -8,8 +10,9 @@ _logger = logging.getLogger(__name__)
 
 class CalendlyWeb(http.Controller):
     
-    @http.route("/calendly", auth="public", type= 'http')
-    def get_result(self, **kwargs):
-        _logger.warning(**kwargs)
-        print(**kwargs)
+    @http.route('/calendly', type='json', auth="none", methods=['POST'], cors="*", csrf=False)
+    def get_result(self, **kw):
+        _logger.info('CONNECTION SUCCESSFUL')
+        _logger.info(http.request.jsonrequest)
+        print(http.request.jsonrequest)
         return
